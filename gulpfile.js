16cb0920cec6bd045755d7acfe39619ffb6c84f7
Thomas Alexander Woolff <t@woolff.dk>
2016-12-21 18:39:55 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var gutil = require('gulp-util');
+var webserver = require('gulp-webserver');
+var cssnano = require('cssnano');
+var precss = require('precss');
+var concat = require('gulp-concat');
+
+var source = 'process/css/';
+var dest = 'builds/';
+
+
+var jsSources = [
+  'process/scripts/jquery-3.1.1.min.js',
+  'process/scripts/script.js'
+];
+
+gulp.task('html', function() {
+    gulp.src(dest + '*.html');
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .on('error', gutil.log)
+    .pipe(gulp.dest(dest + 'js'));
+});
+          
+gulp.task('css', function() {
+    gulp.src(source + 'style.css')
+    .pipe(postcss([
+        autoprefixer(),
+        precss(),
+        cssnano()
+    ]))
+    .on('error', gutil.log)
+    .pipe(gulp.dest(dest + 'css'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch(source + '**/*.css', ['css']); 
+    gulp.watch(dest + '**/*.html', ['html']);
+    gulp.watch(jsSources, ['js']);
+});
+
+gulp.task('webserver', function () {
+    gulp.src(dest)
+    .pipe(webserver({
+        livereload: true,
+        open: true
+    }));
+});
+
+gulp.task('default', ['html', 'css', 'js', 'webserver', 'watch']);
